041fb07ab9b488487f560b0988dda42da3dd7573
ophelia <nswilde@outlook.com>
2023-08-25 09:53:49 +1200

    Initial Commit

---
+++ RockPaperScissors/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  RockPaperScissors
+//
+//  Created by Nikki Wilde on 25/08/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RockPaperScissors/RockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsApp.swift
+//  RockPaperScissors
+//
+//  Created by Nikki Wilde on 25/08/23.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
